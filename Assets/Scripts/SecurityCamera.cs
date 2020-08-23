fe3dbdde7970ead307bb600f76792b8267a458f1
Kain Shepard <kainkun100@gmail.com>
2020-08-22 18:24:58 -0700

    idk

--- Assets/Scripts/SecurityCamera.cs
+++ Assets/Scripts/SecurityCamera.cs
@@ -4,103 +4,23 @@ using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
-    public Transform lightTransform;
-
-    #region movement varaibles
-    public Transform securityCameraAxis;
-    float lightAngle;
-    public Transform angle1;
-    public Transform angle2;
-    public float speed = 1;
-    public float pauseTime = 1;
-    public float maxDistance = 10;
-    public float tension = 0.5f;
-    [Range(0f, 1f)]
-    public float dampen = 0.2f;
-    Transform player;
-    #endregion
-
-    public Color searchingColor;
-    public Color alertColor;
-
-    public MeshRenderer camLensMeshR;
-    Material mat;
-
     private void Awake()
     {
-        mat = camLensMeshR.GetComponent<MeshRenderer>().material;
+
     }
 
     void Start()
     {
-        player = Player.instance.transform;
-        StartCoroutine(camMovement());
-        lightTransform.GetComponent<Light>().range = maxDistance;
-        lightAngle = lightTransform.GetComponent<Light>().spotAngle;
-        lightTransform.GetComponent<Light>().color = searchingColor;
+
     }
 
-    float colorT = 0;
-    float T;
-    float speedT;
-    float targetT;
     void Update()
     {
-        //targetT = (Mathf.Sin(Time.time * 2 * Mathf.PI / loopTime) + 1) / 2;
-        speedT = Mathf.Lerp(speedT, (targetT - T) * tension, dampen);
-        T += speedT;
-        securityCameraAxis.localRotation = Quaternion.SlerpUnclamped(angle1.localRotation, angle2.localRotation, T);
 
-        float playerCameraAngle = Vector3.Angle(player.position - lightTransform.position, lightTransform.forward);
-        RaycastHit hit;
-        if (Physics.Raycast(lightTransform.position, player.position - lightTransform.position, out hit, maxDistance))
-        {
-            if (hit.transform.tag == "Player" && playerCameraAngle <= lightAngle - 20)
-            {
-                colorT += Time.deltaTime * 2;
-                //can see player
-            }
-            else
-            {
-                colorT -= Time.deltaTime * 2;
-            }
-            colorT = Mathf.Clamp01(colorT);
-            lightTransform.GetComponent<Light>().color = Color.Lerp(searchingColor, alertColor, colorT);
-        }
-
-        float emisT = Mathf.InverseLerp(lightAngle, 0, playerCameraAngle);
-        ApplyEmisIntensity(Mathf.Lerp(0.1f, 30, Mathf.Pow(emisT, 3)));
     }
 
     IEnumerator camMovement()
     {
-        while (true)
-        {
-            while (targetT < 1)
-            {
-                targetT += Time.deltaTime * speed;
-                yield return null;
-            }
-            yield return new WaitForSeconds(pauseTime);
-            while (targetT > 0)
-            {
-                targetT -= Time.deltaTime * speed;
-                yield return null;
-            }
-            yield return new WaitForSeconds(pauseTime);
-            yield return null;
-        }
-    }
-
-    void ApplyEmisIntensity(float intensity)
-    {
-        mat.SetFloat("EmisIntensity", intensity * 100f);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(angle1.position, angle1.forward * maxDistance);
-        Gizmos.DrawRay(angle2.position, angle2.forward * maxDistance);
+        yield return new WaitForSeconds(1);
     }
 }
---
+++ Assets/SecurityDroneDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityDroneDoor : MonoBehaviour
+{
+    [SerializeField] GameObject Drone;
+    bool cooldown;
+    public float cooldownTime;
+    float currentCooldownTime;
+
+    private void Update()
+    {
+        if(cooldown)
+            currentCooldownTime += Time.deltaTime;
+        if(currentCooldownTime >= cooldownTime)
+        {
+            cooldown = false;
+            currentCooldownTime = 0;
+        }
+    }
+
+    public void ReleaseDrone()
+    {
+        if (cooldown)
+            return;
+
+
+        print("release");
+        cooldown = true;
+    }
+}
